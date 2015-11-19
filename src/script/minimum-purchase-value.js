1ef57a31f474104d011a9afa65f1d806858c4adf
afonso-praca <afonsoinfo@gmail.com>
2015-11-18 22:17:09 -0200

    bug fix and shows hidden btn on mobile

--- src/script/minimum-purchase-value.js
+++ src/script/minimum-purchase-value.js
@@ -1,11 +1,14 @@
 (function(){
   var subTotal = Number($('.subtotal strong').data('subtotal-valor'));
-  if (subTotal < 20){
-    var diffToMinimum = String((20 - subTotal).toFixed(2)).replace('.', ',');
-    var textDiffToMinimum = 'Nosso pedido mínimo é de <strong>R$ 20,00</strong>. Ainda faltam <strong>R$ '
+  var minimumValue = 19.9;
+  if (subTotal && subTotal < minimumValue){
+    var diffToMinimum = String((minimumValue - subTotal).toFixed(2)).replace('.', ',');
+    var textDiffToMinimum = 'Nosso pedido mínimo é de <strong>R$ 19,90</strong>. Ainda faltam <strong>R$ '
       + diffToMinimum
       + '</strong> para que o pedido possa ser finalizado.';
     var divDiffToMinimum = '<div class="alert alert-diff-to-minimum">' + textDiffToMinimum + '</div>';
     $('.finalizar-compra .caixa-sombreada').after(divDiffToMinimum);
   }
+  // shows continaur comprando on mobile
+  $('.finalizar-compra .acao-editar .botao.hidden-phone').removeClass('hidden-phone')
 })();
