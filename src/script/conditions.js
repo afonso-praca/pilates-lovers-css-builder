d343c578e2e0bf76a692d9ecae75620a3d878c19
afonso <afonsoinfo@gmail.com>
2016-01-26 21:15:46 -0200

    cretaes main offers bar

---
+++ src/script/conditions.js
@@ -0,0 +1,8 @@
+// conditions
+(function(){
+  $('#barraNewsletter').before('<div class="container conditions-container" style="margin-bottom:30px;"><div class="row-fluid">' +
+    '<div class="span12">' +
+    '<img src="https://cdn.awsli.com.br/67/67849/arquivos/FOOTER_ICONES_V01.jpg" class="hidden-phone">' +
+    '<img src="https://cdn.awsli.com.br/67/67849/arquivos/FOOTER_ICONES_V01-mob.jpg" class="visible-phone">' +
+    '</div></div></div>');
+})();
